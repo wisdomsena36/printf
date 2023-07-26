9e3a58fa7e3823fc9fcec828c2316eb0b477ab29
wisdomsena <wisdomsena36@gmail.com>
2023-07-26 16:52:49 -0700

    Fix print_string.c

--- print_string.c
+++ print_string.c
@@ -9,15 +9,17 @@
 int print_string(va_list s)
 {
 	char *my_string;
-	int  i = 0;
+	int i = 0;
 
 	my_string = va_arg(s, char *);
 	if (my_string == NULL)
 		my_string = "(null)";
-	while (my_string[i])
+
+	while (my_string[i] != '\0') // Corrected the condition to check for null terminator.
 	{
 		_putchar(my_string[i]);
 		i++;
 	}
+
 	return (i);
 }
